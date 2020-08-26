8562ebd6e1277c78af94c0ce606ba19ed36b7062
Ryo Fujiwara <frmtricolore@gmail.com>
2020-08-26 23:19:39 +0900

    create layout

--- workplace/src/components/App.js
+++ workplace/src/components/App.js
@@ -1,37 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const App = props => {
-  const [state, setState] = useState(props) 
-
-  const reset = () => setState(props)
-
-  useEffect(() => {
-    console.log('useEffect')
-  })
-
-  useEffect(() => {
-    console.log('useEffect callback once')
-  },[])
-
-  // nameの状態が変化した時だけ呼ばれる
-  useEffect(() => {
-    console.log('This callback is for name only')
-  }, [state.name])
-
+const App = () => {
   return (
     <>
-      <p>現在の{state.name}は{state.price}円です</p>
-      <button onClick={() => setState({...state, price: state.price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: state.price - 1})}>-1</button>
-      <button onClick={() => reset()}>Reset</button>
-      <input value={state.name} onChange={e => setState({...state, name: e.target.value}) }/>
+      <div className="container-fluid">
+        <h4>イベント作成フォーム</h4>
+          <form>
+            <div className="form-group">
+              <label htmlFor="formEventTitle">タイトル</label>
+              <input className="form-control" id="formEventTitle" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="formEventBody">ボディ</label>
+              <textarea className="form-control" id="formEventBody" />
+            </div>
+            <button className="btn btn-primary">イベントを作成する</button>
+            <button className="btn btn-danger">全てのイベントを削除する</button>
+          </form>
+
+          <h4>イベント一覧</h4>
+          <table className="table table-hover">
+            <thead>  
+              <tr>
+                <th>ID</th>
+                <th>タイトル</th>
+                <th>ボディ</th>
+                <th></th>
+              </tr>            
+            </thead>
+            <tbody>
+
+            </tbody>
+
+          </table>
+      </div>
     </>
   )
-}
-
-App.defaultProps = {
-  name: "Sample",
-  price: 1000,
 }
 
 export default App
