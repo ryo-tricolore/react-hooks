0cd108652431013f734aa1b5501ff4f75377ee11
Ryo Fujiwara <frmtricolore@gmail.com>
2020-08-30 16:52:41 +0900

    fix reducer

--- workplace/src/reducers/index.js
+++ workplace/src/reducers/index.js
@@ -5,16 +5,16 @@
 // }
 
 const events = (state = [], action) => {
-    switch(action.type) {
-        case 'CREATE EVENT':
+    switch(action.type) {        
+        case 'CREATE_EVENT':            
             const event = { title: action.title, body: action.body }
             const length = state.length;
             const id = length === 0 ? 1 : state[length - 1].id + 1;
         
             return [...state, {id, ...event}];
-        case 'DELETE EVENT':
+        case 'DELETE_EVENT':
             return state;    
-        case 'DELETE ALL EVENT':
+        case 'DELETE_ALL_EVENT':
             return [];        
         default:
             return state;
