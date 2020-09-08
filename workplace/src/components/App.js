13bba5a5205b38250c7f110f3c2ca08c97b79590
Ryo Fujiwara <frmtricolore@gmail.com>
2020-09-08 23:01:35 +0900

    use react-context

--- workplace/src/components/App.js
+++ workplace/src/components/App.js
@@ -5,14 +5,18 @@ import reducer from '../reducers'
 import EventForm from './EventForm'
 import Events from './Events'
 
+import AppContext from '../contexts/AppContext'
+
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);  
   return (
-    <>      
-      <div className="container-fluid">
-          <EventForm state={state} dispatch={dispatch}/>
-          <Events state={state} dispatch={dispatch}/>
-      </div>
+    <> 
+      <AppContext.Provider value={'Hello'}>
+        <div className="container-fluid">
+            <EventForm state={state} dispatch={dispatch}/>
+            <Events state={state} dispatch={dispatch}/>
+        </div>
+      </AppContext.Provider>
     </>
   )
 }
